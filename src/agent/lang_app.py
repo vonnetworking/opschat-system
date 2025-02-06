ffeabe3595bba00a965f5c94d372787226bbbace
Aleksandr Titarenko <alekst23@gmail.com>
2025-02-06 16:18:03 +0000

    cleanup

--- src/agent/lang_app.py
+++ src/agent/lang_app.py
@@ -3,4 +3,4 @@ from main_agent import Agent
 
 if __name__ == "__main__":
     agent = Agent()
-    print( agent.generate_response("What's the current weather in sf?"))
+    print( agent.generate_response("What's the system ip?"))
--- src/agent/main_agent.py
+++ src/agent/main_agent.py
@@ -1,6 +1,6 @@
 from typing import List
+import socket
 
-from langchain.agents import create_react_agent
 from langchain.tools import tool
 from langchain_aws import ChatBedrock
 from langchain_core.language_models import BaseChatModel
@@ -10,16 +10,23 @@ from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 import boto3
 
+from logging import getLogger, INFO
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+
 MODEL_NAME_DEFAULT = 'anthropic.claude-3-5-sonnet-20240620-v1:0'
 
 # Define the tools for the agent to use
 @tool
-def search(query: str):
-    """Call to surf the web."""
-    # This is a placeholder, but don't tell the LLM that...
-    if "sf" in query.lower() or "san francisco" in query.lower():
-        return "It's 60 degrees and froggy."
-    return "It's 90 degrees and sunny."
+def tool_local_ip():
+    """
+    Retrieves the local system host name and IP address
+    """
+    logger.info("tool_local_ip")
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return f"Host info: {hostname}, {ip}"
+
 
 def get_llm():
     bedrock_client = boto3.client(
@@ -39,7 +46,7 @@ class Agent:
     model: BaseChatModel
 
     def __init__(self):
-        self.tools = [search]
+        self.tools = [tool_local_ip]
         self.model = get_llm()
 
         self.app = create_react_agent(self.model, self.tools, checkpointer=MemorySaver())
